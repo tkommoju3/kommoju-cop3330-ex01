3e89133ebda95af43af174b00751ee14dc432af0
tanushka <tanushkakommoju@knights.ucf.edu>
2021-06-05 19:49:21 -0400

    first exercise

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner (System.in);
+        System.out.print("What is your name? ");
+        String name = input.next();
+        System.out.println();
+        System.out.println("Hello, "+name+", nice to meet you!");
+    }
+}
